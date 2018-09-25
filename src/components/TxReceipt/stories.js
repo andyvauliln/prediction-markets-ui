183254356b7c68f63ff2a145a54291022d151cfc
Andrei Vaulin <andy.vaulin@gmail.com>
2018-09-25 18:11:16 +0300

    added tx receipt

--- src/components/TxReceipt/stories.js
+++ src/components/TxReceipt/stories.js
@@ -1,12 +1,10 @@
 // @flow
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs/react';
+import { withKnobs } from '@storybook/addon-knobs/react';
 import { withInfo } from '@storybook/addon-info';
 import TxReceipt from './TxReceipt';
 import { receipt } from '../../data';
-
-import '../../styles/reset.css';
 
 storiesOf('TxReceipt', module)
   .addDecorator(withKnobs)
