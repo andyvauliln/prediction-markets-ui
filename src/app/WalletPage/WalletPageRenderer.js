24f4a43f6c6ff5f1acab1302aadfcee479e688a1
Andrei Vaulin <andy.vaulin@gmail.com>
2018-09-25 17:59:55 +0300

    created wallet page

---
+++ src/app/WalletPage/WalletPageRenderer.js
@@ -0,0 +1,106 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+import { Card } from '@blueprintjs/core';
+import CenteredSpinner from '../../components/Common/CenteredSpinner';
+import DepositTable from '../../components/DepositTable';
+import CurrentWallet from '../../components/CurrentWallet';
+import Notifier from '../../components/Notifier';
+
+import type { TokenData } from '../../types/tokens';
+
+type Props = {
+  loading: boolean,
+  pvtKeyLocked: boolean,
+  accountAddress: string,
+  accountPrivateKey: string,
+  etherBalance: string,
+  gasPrice: number,
+  gas: number,
+  //Deposit table props
+  provider: string,
+  depositTableData: Array<TokenData>,
+  toggleAllowance: string => void,
+};
+
+const WalletPageRenderer = ({
+  loading,
+  pvtKeyLocked,
+  provider,
+  depositTableData,
+  accountAddress,
+  accountPrivateKey,
+  etherBalance,
+  gasPrice,
+  gas,
+  toggleAllowance,
+}: Props) => {
+  return (
+    <Wrapper>
+      <RowWrapper>
+        <CurrentWallet
+          pvtKeyLocked={pvtKeyLocked}
+          accountAddress={accountAddress}
+          accountPrivateKey={accountPrivateKey}
+          etherBalance={etherBalance}
+          gasPrice={gasPrice}
+          gas={gas}
+        />
+        <WalletPageContent>
+          {loading ? (
+            <CenteredSpinner />
+          ) : (
+            <RightSection>
+              <DepositTableWrapper>
+                <DepositTable
+                  provider={provider}
+                  toggleAllowance={toggleAllowance}
+                  depositTableData={depositTableData}
+                />
+              </DepositTableWrapper>
+              <HeadingMenu>
+                <h4>Heading</h4>
+              </HeadingMenu>
+            </RightSection>
+          )}
+        </WalletPageContent>
+      </RowWrapper>
+      <Notifier />
+    </Wrapper>
+  );
+};
+
+const RowWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const HeadingMenu = styled.div`
+  width: 30%;
+`;
+
+const DepositTableWrapper = styled.div`
+  height: 100%;
+  width: 69%;
+`;
+const RightSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+  width: 100%;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 0.5em;
+  padding-right: 0.5em;
+  padding-top: 0.5em;
+`;
+
+const WalletPageContent = styled(Card)`
+  height: 92vh;
+  width: 75%;
+  margin: 0.5em;
+`;
+
+export default WalletPageRenderer;
---
+++ src/app/WalletPage/connect.js
@@ -0,0 +1,52 @@
+import { connect } from 'react-redux';
+import walletPageSelector, {
+  queryAccountData,
+  toggleAllowance,
+} from '../../store/models/walletPage';
+import settingsPageSelector from '../../store/models/settings';
+import accountSelector from '../../store/models/layout';
+import { removeNotification } from '../../store/actions/app';
+
+export function mapStateToProps(state, props) {
+  const { authenticated } = accountSelector(state);
+  const { pvtKeyLocked } = settingsPageSelector(state);
+
+  const {
+    depositTableData,
+    accountAddress,
+    currentBlock,
+    etherBalance,
+    provider,
+    accountPrivateKey,
+    gas,
+    gasPrice,
+  } = walletPageSelector(state);
+
+  const loading = !(depositTableData.length > 0);
+
+  return {
+    depositTableData,
+    accountAddress,
+    accountPrivateKey,
+    loading,
+    isDefaultAccountSet: false,
+    authenticated,
+    currentBlock,
+    provider,
+    etherBalance,
+    pvtKeyLocked,
+    gas,
+    gasPrice,
+  };
+}
+
+export const mapDispatchToProps = {
+  queryAccountData,
+  removeNotification,
+  toggleAllowance,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+);
