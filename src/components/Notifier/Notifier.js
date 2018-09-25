3f4f56a14e1bed1874b08a3ac8e264952e94241c
Andrei Vaulin <andy.vaulin@gmail.com>
2018-09-25 18:07:03 +0300

    added notifier

---
+++ src/components/Notifier/Notifier.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Position, Toaster } from '@blueprintjs/core';
+
+// eslint-disable-next-line
+type Props = {
+  lastNotification: Object,
+  removeNotification: number => void,
+};
+
+class Notifier extends React.Component {
+  show = notification => {
+    let { intent, message } = notification;
+    Notification.show({ intent: intent || 'success', message: message });
+
+    this.props.removeNotification(this.props.lastNotification.id);
+  };
+
+  render() {
+    let { lastNotification } = this.props;
+    if (lastNotification) lastNotification.id && this.show(lastNotification);
+
+    return null;
+  }
+
+  componentDidUpdate(prevProps) {
+    let { lastNotification, removeNotification } = prevProps;
+    if (lastNotification) removeNotification(lastNotification.id);
+  }
+}
+
+const Notification = Toaster.create({
+  position: Position.TOP_RIGHT,
+});
+
+export default Notifier;
