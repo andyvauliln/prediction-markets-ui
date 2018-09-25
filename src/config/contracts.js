b30c3d4d88903ea35a708b2c3e41b8f1e69e1f6b
Andrei Vaulin <andy.vaulin@gmail.com>
2018-09-25 18:12:44 +0300

    changed congigurations

---
+++ src/config/contracts.js
@@ -0,0 +1,19 @@
+import addresses from './addresses.json';
+
+export const EXCHANGE_ADDRESS = {
+  '1': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '2': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '3': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '4': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '1000': addresses[1000]['Exchange'],
+  '8888': addresses[8888]['Exchange'],
+};
+
+export const WETH_ADDRESS = {
+  '1': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '2': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '3': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '4': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '1000': addresses[1000]['WETH'],
+  '8888': addresses[8888]['WETH'],
+};
---
+++ src/config/quotes.js
@@ -0,0 +1,58 @@
+import addresses from './addresses.json';
+
+const networkID = process.env.REACT_APP_DEFAULT_NETWORK_ID || 'default';
+
+const quoteTokensTable = {
+  '8888': [
+    { symbol: 'WETH', address: addresses['8888']['WETH'] },
+    { symbol: 'MKR', address: addresses['8888']['MKR'] },
+  ],
+  '1000': [
+    { symbol: 'WETH', address: addresses['1000']['WETH'] },
+    { symbol: 'MKR', address: addresses['1000']['MKR'] },
+  ],
+  default: [
+    { symbol: 'WETH', address: '0x549638ff7b1038a1923f8e2c38b8c6fc50b8acb6' },
+    { symbol: 'DAI', address: '0x77da6a1ea1cf893ac9941f08bf9714b63c90298d' },
+  ],
+};
+
+const quoteTokensBySymbolsTable = {
+  '8888': {
+    WETH: {
+      symbol: 'WETH',
+      address: addresses['8888']['WETH'],
+    },
+    MKR: {
+      symbol: 'MKR',
+      address: addresses['8888']['MKR'],
+    },
+  },
+  '1000': {
+    WETH: {
+      symbol: 'WETH',
+      address: addresses['1000']['WETH'],
+    },
+    MKR: {
+      symbol: 'MKR',
+      address: addresses['1000']['MKR'],
+    },
+  },
+  default: {
+    WETH: {
+      symbol: 'WETH',
+      address: '0x549638ff7b1038a1923f8e2c38b8c6fc50b8acb6',
+    },
+    DAI: {
+      symbol: 'DAI',
+      address: '0x77da6a1ea1cf893ac9941f08bf9714b63c90298d',
+    },
+  },
+};
+
+export const quoteTokensBySymbols = quoteTokensBySymbolsTable[networkID];
+export const quoteTokenSymbols = Object.keys(quoteTokensBySymbols);
+export const quoteTokens = quoteTokensTable[process.env.REACT_APP_DEFAULT_NETWORK_ID].map((m, index) => ({
+  ...m,
+  rank: index + 1,
+}));
